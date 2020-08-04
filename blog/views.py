182ff03ee8e0c686c324be110c505b436f3732c4
anine <anine0923@gmail.com>
2020-08-04 23:58:45 +0800

    1
    4-3

--- blog/views.py
+++ blog/views.py
@@ -22,3 +22,21 @@ def article_content(request):
                                                                     publish_data)
 
     return HttpResponse(return_str)
+
+def get_index_page(request):
+    all_article = Article.objects.all()
+    return render(request, 'blog/index.html',
+                  {
+                      'article_list':all_article
+                  }
+                  )
+
+def get_detail_page(request):
+    curr_article = Article.objects.all()[2]
+    section_list = curr_article.content.split('\n')
+    return render(request, 'blog/detail.html',
+                  {
+                      'curr-article':curr_article,
+                      'section_list':section_list
+                  }
+                  )
