87a3f87de55442c46457568ab3d848ae31b35188
anine <anine0923@gmail.com>
2020-08-06 23:30:21 +0800

    添加分页功能

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Article
 
+
+from django.core.paginator import Paginator
 # Create your views here.
 
 def hello_world(request):
@@ -24,10 +26,34 @@ def article_content(request):
     return HttpResponse(return_str)
 
 def get_index_page(request):
+    page = request.GET.get('page')
+    if(page):
+        page = int(page)
+    else:
+        page = 1
+
     all_article = Article.objects.all()
+
+    paginator = Paginator(all_article,2)
+    page_num = paginator.num_pages
+    print('page num:',paginator.num_pages)
+    page_article_list = paginator.page(page)
+    if page_article_list.has_next():
+        next_page = page +1
+    else:
+        next_page = page
+    if page_article_list.has_previous():
+        previous_page = page - 1
+    else:
+        previous_page = page
+
     return render(request, 'blog/index.html',
                   {
-                      'article_list':all_article
+                      'article_list': page_article_list,
+                      'page_num': range(1, page_num + 1),
+                      'curr_page': page,
+                      'next_page': next_page,
+                      'previous': previous_page
                   }
                   )
 
